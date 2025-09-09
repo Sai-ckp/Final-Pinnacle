fb558f3b58c8f2cd166119dc487ddfadf282ef84
Sai-ckp <saishashank@ckpsoftware.com>
2025-09-09 09:21:29 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-touch /home/site/wwwroot/started.txt
-source /home/site/wwwroot/venv/bin/activate
-python manage.py migrate
-gunicorn --bind=0.0.0.0:8000 student_alerts_app.wsgi:application
 
+
+# Avoid writing to read-only directory
+touch /home/started.txt
+
+# Install dependencies
+pip install -r /home/site/wwwroot/requirements.txt
+
+# Apply database migrations
+python /home/site/wwwroot/manage.py migrate
+
+# Start the Django app using gunicorn
+gunicorn --chdir /home/site/wwwroot/student_alerts_app student_alerts_app.wsgi:application --bind=0.0.0.0:8000
